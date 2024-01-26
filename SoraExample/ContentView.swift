50f8dc51beaddb4ba3b0b967f4a07bd24ea137a8
Mercen <mercen@mercen.net>
2024-01-27 05:52:13 +0900

    Initial Commit

---
+++ SoraExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoraExample
+//
+//  Created by Mercen on 1/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoraExample/SoraExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoraExampleApp.swift
+//  SoraExample
+//
+//  Created by Mercen on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SoraExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
